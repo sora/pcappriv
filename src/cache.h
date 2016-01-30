690c7ed460a0b36cda6e578430b6960301bfb9ce
sora <sora@haeena.net>
2016-01-30 23:16:16 +0900

    fix

--- src/cache.h
+++ src/cache.h
@@ -30,17 +30,25 @@ KHASH_MAP_INIT_INT(iv6, hash6_t)
 khash_t(iv4) *h4;
 khash_t(iv6) *h6;
 
-
-static inline uint32_t addr6_hash(const struct in6_addr ip6) {
-	return (ip6.s6_addr32[3] ^ ip6.s6_addr32[2] ^
-	        ip6.s6_addr32[1] ^ ip6.s6_addr32[0]);
+static inline uint32_t addr6_hash(const struct in6_addr *a) {
+	return (a->s6_addr32[3] ^ a->s6_addr32[2] ^
+	        a->s6_addr32[1] ^ a->s6_addr32[0]);
 }
 
-static inline uint32_t addr6_eq(const struct in6_addr a, const struct in6_addr b) {
-	return ((a.s6_addr32[3] == b.s6_addr32[3]) &&
-	        (a.s6_addr32[2] == b.s6_addr32[2]) &&
-	        (a.s6_addr32[1] == b.s6_addr32[1]) &&
-	        (a.s6_addr32[0] == b.s6_addr32[0]));
+#if 0
+static inline uint32_t addr6_hash(const struct in6_addr *a) {
+	const unsigned long *ul = (const unsigned long *)a;
+	unsigned long x = ul[0] ^ ul[1];
+	return (uint32_t)(x ^ (x >> 32));
+}
+#endif
+
+
+static inline uint32_t addr6_eq(const struct in6_addr *a, const struct in6_addr *b) {
+	return ((a->s6_addr32[3] == b->s6_addr32[3]) &&
+	        (a->s6_addr32[2] == b->s6_addr32[2]) &&
+	        (a->s6_addr32[1] == b->s6_addr32[1]) &&
+	        (a->s6_addr32[0] == b->s6_addr32[0]));
 }
 
 #endif /* _hash_h_ */
